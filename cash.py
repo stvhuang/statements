1537b62061f1e8cf0d0b48950c6aca171a78c4c3
Steven Yan-Jia Huang <s@stvhuang.me>
2026-01-19 21:54:39 +0800

    chore: update

---
+++ cash.py
@@ -0,0 +1,37 @@
+# IDR (Indonesian Rupiah)
+idr = 0
+idr += 50000 * 39
+idr += 10000 * 1
+idr += 5000 * 6
+idr += 2000 * 7
+idr += 1000 * 2
+idr += 500 * 6
+idr += 200 * 5
+idr += 100 * 1
+print(f"{idr=}")
+
+# IND (Indian Rupee)
+ind = 0
+ind += 500 * 30
+ind += 200 * 2
+ind += 100 * 5
+ind += 50 * 2
+ind += 20 * 1
+ind += 10 * 8
+ind += 5 * 2
+ind += 2 * 2
+ind += 1 * 1
+print(f"{ind=}")
+
+# NZD (New Zealand Dollar)
+nzd = 0
+nzd += 5 * 2
+nzd += 10 * 3
+nzd += 50 * 8
+nzd += 20 * 17
+nzd += 2 * 13
+nzd += 1 * 11
+nzd += 0.5 * 6
+nzd += 0.2 * 6
+nzd += 0.1 * 4
+print(f"{nzd=}")
